f9ba91fb8996d834834274e2abaf163a8dc527c6
alegich <31540570+alegich@users.noreply.github.com>
2017-09-01 15:56:22 +0200

    Add files via upload

---
+++ TimeMeasurer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace timecalclib
+{
+   public class TimeMeasurer: Measurable
+   {
+      public TimeMeasurer(Writable writer)
+      {
+         this.writer = writer;
+      }
+
+      public void DeviceLocked()
+      {
+         writer.WriteLock(DateTime.UtcNow);
+      }
+
+      public void DeviceUnlocked()
+      {
+         writer.WriteUnlock(DateTime.UtcNow);
+      }
+
+      public void MeasureStarted()
+      {
+         writer.WriteStart(DateTime.UtcNow);
+      }
+
+      public void MeasureStopped()
+      {
+         writer.WriteStop(DateTime.UtcNow);
+      }
+
+      public void DeviceTurnedOff()
+      {
+         writer.WriteLock(DateTime.UtcNow);
+      }
+
+      private readonly Writable writer;
+   }
+}
---
+++ WeekReporter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace timecalclib
+{
+   public class WeekReporter: WeekReportable
+   {
+      public WeekReporter(List<Reportable> records)
+      {
+         this.records = records;
+      }
+
+      public TimeSpan WholeWeekTime()
+      {
+         throw new NotImplementedException();
+      }
+
+      public TimeSpan TimeLeft()
+      {
+         TimeSpan retVal = new TimeSpan();
+         foreach (Reportable item in records)
+         {
+            retVal += item.WholeDayTime();
+         }
+
+         return new TimeSpan(records.Count * 8, 0, 0) - retVal;
+      }
+
+      private readonly List<Reportable> records;
+   }
+}
