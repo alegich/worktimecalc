245377a85a5f61965ebdd5e6838ca64758066d94
buka\olegich <donot@mail.me>
2017-09-18 19:10:49 +0200

    timecalcmq lib added

---
+++ timecalcmq/TimeCalcFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using timecalclib;
+
+namespace timecalcmq
+{
+   public class TimeCalcFactory : timecalclib.TimeCalcFactory
+   {
+      public Reportable CreateReporter(DateTime day)
+      {
+         throw new NotImplementedException();
+      }
+
+      public WeekReportable CreateWeekReporter(DateTime date)
+      {
+         throw new NotImplementedException();
+      }
+
+      public Writable CreateWriter()
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
